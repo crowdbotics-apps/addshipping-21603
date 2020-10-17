42947ea2bdb8505c4b6e2a9c31b77554a5488f01
Crowdbotics <team@crowdbotics.com>
2020-10-17 01:52:41 +0000

    Update models

--- backend/addshipping_21603/wsgi.py
+++ backend/addshipping_21603/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'addshipping_21603.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "addshipping_21603.settings")
 
 application = get_wsgi_application()
